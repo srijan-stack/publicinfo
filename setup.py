5e96d9f59bca61b56facbc808e5fa6e4edcc2656
aSrijan <adhyayansrijan1998@gmail.com>
2022-12-20 16:52:19 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name="prvtinfo",
+      version="0.1",
+      url='https://github.com/srijan-stack/prvtinfo',
+      description="this is a info getter",
+      long_description="use this to get info",
+      author="srijan",
+      packages=["src"],
+      install_requires=[])
---
+++ src/info.py
@@ -0,0 +1,5 @@
+def weight(num):
+    return num
+
+def height(num):
+    return num
